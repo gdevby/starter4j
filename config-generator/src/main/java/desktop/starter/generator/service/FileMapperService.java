3a08705b436d2165045cacc4ec7ea5cf017f5cab
aleksandr <aleksandr@computer-1>
2021-08-02 16:04:58 +0300

    Изменен FileMapperService

--- config-generator/src/main/java/desktop/starter/generator/service/FileMapperService.java
+++ config-generator/src/main/java/desktop/starter/generator/service/FileMapperService.java
@@ -6,35 +6,29 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.apache.commons.io.FileUtils;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
+
+import desktop.starter.generator.Main;
 
 public class FileMapperService {
-	//todo used one gson from main
-    static Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-    static Charset charset = StandardCharsets.UTF_8;
 
-	public static void write(Object create, Path config) throws FileNotFoundException, IOException {
+	public void write(Object create, Path config) throws FileNotFoundException, IOException {
 		if (Files.notExists(config.getParent()))
 			Files.createDirectories(config.getParent());
-		try (OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(config.toFile()), charset)) {
-			GSON.toJson(create, out);
+		try (OutputStreamWriter out = new OutputStreamWriter(new FileOutputStream(config.toFile()), Main.charset)) {
+			Main.GSON.toJson(create, out);
 		}
 	}
 
-	public static Object read(Path file, Class<?> clas) throws FileNotFoundException, IOException {
+	public Object read(Path file, Class<?> clas) throws FileNotFoundException, IOException {
 		try (BufferedReader read = new BufferedReader(new FileReader(file.toFile())) ){
-			return GSON.fromJson(read, clas);
+			return Main.GSON.fromJson(read, clas);
 		}
 	}
 
-	public static void copyFile(Path source, Path dest) throws IOException {
+	public void copyFile(Path source, Path dest) throws IOException {
 	    FileUtils.copyDirectory(source.toFile(), dest.toFile());
 	}
-
 }
