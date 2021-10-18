b6bef2730fd70f11f11214f5fa5adcbc98150ae4
aleksandr <aleksandr@work-1>
2021-10-18 14:24:10 +0300

    moved JVMConfin from config-generator in starter-core

--- starter-core/src/main/java/by/gdev/model/JVMConfig.java
+++ starter-core/src/main/java/by/gdev/model/JVMConfig.java
@@ -1,4 +1,4 @@
-package by.gdev.generator.model;
+package by.gdev.model;
 
 import java.util.Map;
 
@@ -6,6 +6,7 @@ import by.gdev.util.OSInfo.Arch;
 import by.gdev.util.OSInfo.OSType;
 import by.gdev.util.model.download.Repo;
 import lombok.Data;
+
 /**
  * Used to create config for any operation system and arch of the system to download jvm config.
  * Example we find a link for windows -> x32 -> jre-default -> jre-8u281-linux-i586.
@@ -17,3 +18,4 @@ import lombok.Data;
 public class JVMConfig {
 	private Map<OSType, Map<Arch, Map<String,Repo>>> jvms;
 }
+
