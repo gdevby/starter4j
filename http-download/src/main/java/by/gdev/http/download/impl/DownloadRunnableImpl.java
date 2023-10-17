09559e4a7828259da495164568dd7dc1e26abb77
alexander <alexander@alexander-desktop>
2023-10-17 09:23:28 +0300

    added check on 20X response code

--- http-download/src/main/java/by/gdev/http/download/impl/DownloadRunnableImpl.java
+++ http-download/src/main/java/by/gdev/http/download/impl/DownloadRunnableImpl.java
@@ -13,6 +13,7 @@ import java.util.Queue;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpEntity;
+import org.apache.http.StatusLine;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -41,14 +42,15 @@ public class DownloadRunnableImpl implements Runnable {
 	private EventBus eventBus;
 	private int DEFAULT_MAX_ATTEMPTS = 3;
 
-	public DownloadRunnableImpl(Queue<DownloadElement> downloadElements, List<DownloadElement> processedElements, CloseableHttpClient httpclient, RequestConfig requestConfig, EventBus eventBus) {
+	public DownloadRunnableImpl(Queue<DownloadElement> downloadElements, List<DownloadElement> processedElements,
+			CloseableHttpClient httpclient, RequestConfig requestConfig, EventBus eventBus) {
 		this.downloadElements = downloadElements;
 		this.processedElements = processedElements;
 		this.httpclient = httpclient;
 		this.requestConfig = requestConfig;
 		this.eventBus = eventBus;
 	}
-	
+
 	@Override
 	public void run() {
 		while (status.equals(DownloaderStatusEnum.WORK)) {
@@ -60,8 +62,9 @@ public class DownloadRunnableImpl implements Runnable {
 					try {
 						download(element);
 						element.getHandlers().forEach(post -> post.postProcessDownloadElement(element));
-					}catch(Throwable e1) {
-						element.setError(new UploadFileException(element.getRepo().getRepositories().get(0) + element.getMetadata().getRelativeUrl(),
+					} catch (Throwable e1) {
+						element.setError(new UploadFileException(
+								element.getRepo().getRepositories().get(0) + element.getMetadata().getRelativeUrl(),
 								element.getMetadata().getPath(), e1.getLocalizedMessage()));
 					}
 				} else {
@@ -74,60 +77,66 @@ public class DownloadRunnableImpl implements Runnable {
 
 	/**
 	 * A method that allows a byte-by-byte download of a file at the specified URI
+	 * 
 	 * @param element item received from the download queue
 	 * @throws IOException
 	 * @throws InterruptedException
 	 */
-	
+
 	private void download(DownloadElement element) throws IOException, InterruptedException {
 		processedElements.add(element);
 		File file = new File(element.getPathToDownload() + element.getMetadata().getPath());
-			for (int attempt = 0; attempt < DEFAULT_MAX_ATTEMPTS; attempt++) {
+		for (int attempt = 0; attempt < DEFAULT_MAX_ATTEMPTS; attempt++) {
+			try {
+				BufferedInputStream in = null;
+				BufferedOutputStream out = null;
+				boolean resume = false;
+				String url = element.getRepo().getRepositories().get(0) + element.getMetadata().getRelativeUrl();
+				HttpGet httpGet = new HttpGet(url);
+				log.trace(String.valueOf(httpGet));
 				try {
-					BufferedInputStream in = null;
-					BufferedOutputStream out = null;
-					boolean resume = false;
-					HttpGet httpGet = new HttpGet(element.getRepo().getRepositories().get(0) + element.getMetadata().getRelativeUrl());
-					log.trace(String.valueOf(httpGet));
-					try {
-						if (!file.getParentFile().exists())
-							file.getParentFile().mkdirs();
-						if (file.exists() && Objects.nonNull(element.getMetadata().getSha1())
-								&& file.length() != element.getMetadata().getSize()) {
-							httpGet.addHeader("Range",
-									"bytes= " + file.length() + "-" + element.getMetadata().getSize());
-							resume = true;
-						}
-						httpGet.setConfig(requestConfig);
-						CloseableHttpResponse response = httpclient.execute(httpGet);
-						HttpEntity entity = response.getEntity();
-						in = new BufferedInputStream(entity.getContent());
-						out = new BufferedOutputStream(new FileOutputStream(file, resume));
-						
-						byte[] buffer = new byte[1024];
-						int curread = in.read(buffer);
-						while (curread != -1) {
-							if (status.equals(DownloaderStatusEnum.CANCEL)) {
-								break;
-							} else {
-								out.write(buffer, 0, curread);
-								curread = in.read(buffer);
-								element.setDownloadBytes(element.getDownloadBytes() + curread);
-							}
-						}
-						eventBus.post(element);
-						LocalTime endTime = LocalTime.now();
-						element.setEnd(endTime);
-						DEFAULT_MAX_ATTEMPTS = 1;
-					} finally {
-						httpGet.abort();
-						IOUtils.close(out);
-						IOUtils.close(in);
+					if (!file.getParentFile().exists())
+						file.getParentFile().mkdirs();
+					if (file.exists() && Objects.nonNull(element.getMetadata().getSha1())
+							&& file.length() != element.getMetadata().getSize()) {
+						httpGet.addHeader("Range", "bytes= " + file.length() + "-" + element.getMetadata().getSize());
+						resume = true;
 					}
-				} catch (SocketTimeoutException e) {
-					if (attempt == DEFAULT_MAX_ATTEMPTS)
-						throw new SocketTimeoutException();
+					httpGet.setConfig(requestConfig);
+					CloseableHttpResponse response = httpclient.execute(httpGet);
+					StatusLine sl = response.getStatusLine();
+					String responseCode = String.valueOf(sl.getStatusCode());
+					if (!responseCode.startsWith("20"))
+						throw new IOException(
+								String.format("code %s phrase %s %s", responseCode, sl.getReasonPhrase(), url));
+					HttpEntity entity = response.getEntity();
+					in = new BufferedInputStream(entity.getContent());
+					out = new BufferedOutputStream(new FileOutputStream(file, resume));
+
+					byte[] buffer = new byte[1024];
+					int curread = in.read(buffer);
+					while (curread != -1) {
+						if (status.equals(DownloaderStatusEnum.CANCEL)) {
+							break;
+						} else {
+							out.write(buffer, 0, curread);
+							curread = in.read(buffer);
+							element.setDownloadBytes(element.getDownloadBytes() + curread);
+						}
+					}
+					eventBus.post(element);
+					LocalTime endTime = LocalTime.now();
+					element.setEnd(endTime);
+					DEFAULT_MAX_ATTEMPTS = 1;
+				} finally {
+					httpGet.abort();
+					IOUtils.close(out);
+					IOUtils.close(in);
 				}
-			}	
+			} catch (SocketTimeoutException e) {
+				if (attempt == DEFAULT_MAX_ATTEMPTS)
+					throw new SocketTimeoutException();
+			}
+		}
 	}
 }
