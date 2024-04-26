8ce22e568002d0125d51ff7898f09c14b5e792da
Paylek01 <pggsviryd@gmail.com>
2024-04-26 15:45:12 +0300

    Fix bug with Chine language

--- starter-core/src/main/java/by/gdev/handler/Localise.java
+++ starter-core/src/main/java/by/gdev/handler/Localise.java
@@ -11,20 +11,24 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Localise {
-	
+
 	static Locale locale;
 
-	public Locale getLocal(){
+	public Locale getLocal() {
 		try {
 			Properties property = new Properties();
 			ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-	        InputStream inputStream = classloader.getResourceAsStream("application.properties");
+			InputStream inputStream = classloader.getResourceAsStream("application.properties");
 			property.load(inputStream);
 			String language = property.getProperty("language");
 			String[] parts = language.split(",");
-			if (ArrayUtils.contains(parts, Locale.getDefault().getLanguage()))
-				locale = new Locale.Builder().setLanguage(Locale.getDefault().getLanguage()).build();
-			else
+			if (ArrayUtils.contains(parts, Locale.getDefault().getLanguage())) {
+				if (Locale.getDefault().getLanguage().contains("_")) {
+					String[] ar = Locale.getDefault().getLanguage().split("_");
+					locale = new Locale(ar[0], ar[1]);
+				} else
+					locale = new Locale.Builder().setLanguage(Locale.getDefault().getLanguage()).build();
+			} else
 				locale = new Locale.Builder().setLanguage("en").build();
 		} catch (IOException e) {
 			log.error("Error", e);
