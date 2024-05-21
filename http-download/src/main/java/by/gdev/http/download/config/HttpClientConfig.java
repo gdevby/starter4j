d473550e07e5c8ba5533abe4071b362aaf829f80
Paylek01 <pggsviryd@gmail.com>
2024-05-21 13:25:28 +0300

    set numbers for read and established connection.

--- http-download/src/main/java/by/gdev/http/download/config/HttpClientConfig.java
+++ http-download/src/main/java/by/gdev/http/download/config/HttpClientConfig.java
@@ -2,24 +2,29 @@ package by.gdev.http.download.config;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+
 /**
- * This class allows you to set the maximum number of total open connections, the maximum number of concurrent connections per route and connection Keep-Alive strategy.
+ * This class allows you to set the maximum number of total open connections,
+ * the maximum number of concurrent connections per route and connection
+ * Keep-Alive strategy.
+ * 
  * @author Robert Makrytski
  *
  */
 public class HttpClientConfig {
-
 	public static CloseableHttpClient getInstanceHttpClient() {
 		PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
 		cm.setDefaultMaxPerRoute(5);
 		cm.setMaxTotal(20);
+		RequestConfig config = RequestConfig.custom().setConnectTimeout(60 * 1000).setSocketTimeout(60 * 1000).build();
 		CloseableHttpClient builder = HttpClients.custom()
 				.setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE).setConnectionManager(cm)
-				.evictIdleConnections(10, TimeUnit.SECONDS).build();
+				.setDefaultRequestConfig(config).evictIdleConnections(10, TimeUnit.SECONDS).build();
 		return builder;
 	}
 }
