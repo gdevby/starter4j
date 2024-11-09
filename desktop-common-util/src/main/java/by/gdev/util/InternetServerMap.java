914daae321e7376061093a426f25eb65bcdcf2ea
robert Makrytski <robert.makrytski@gmail.com>
2024-11-09 16:21:02 +0300

    send request when it does't have internet, because it can work again

--- desktop-common-util/src/main/java/by/gdev/util/InternetServerMap.java
+++ desktop-common-util/src/main/java/by/gdev/util/InternetServerMap.java
@@ -2,13 +2,21 @@ package by.gdev.util;
 
 import java.util.HashMap;
 
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class InternetServerMap extends HashMap<String, Boolean> {
 	private static final long serialVersionUID = 1L;
+	/**
+	 * We can ignore skipped url without internet. Because it can restore.
+	 */
+	@Setter
+	private boolean availableInternet;
 
 	public boolean isSkippedURL(String url) {
+		if (!availableInternet)
+			return false;
 		if (keySet().stream().filter(e -> url.contains(e) && !this.get(e)).findAny().isPresent()) {
 			log.debug("skip request to server {}", url);
 			return true;
