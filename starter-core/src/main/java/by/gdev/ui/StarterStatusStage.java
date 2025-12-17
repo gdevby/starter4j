97ae1ecf4c8935cca259b23f17212425abdc1efb
Vadim Semashko <vadim.semashko05@gmail.com>
2025-12-17 18:13:18 +0300

    changed status interface

--- starter-core/src/main/java/by/gdev/ui/StarterStatusStage.java
+++ starter-core/src/main/java/by/gdev/ui/StarterStatusStage.java
@@ -2,27 +2,20 @@ package by.gdev.ui;
 
 import by.gdev.http.upload.download.downloader.DownloaderStatus;
 import by.gdev.http.upload.download.downloader.DownloaderStatusEnum;
-import by.gdev.util.DesktopUtil;
-import by.gdev.util.OSInfo;
 import com.google.common.eventbus.Subscribe;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Cursor;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseButton;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.text.Font;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -30,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
@@ -37,90 +31,73 @@ import java.util.ResourceBundle;
 public class StarterStatusStage extends Stage {
     private static final long serialVersionUID = 1L;
     private ProgressBar progressBar;
-    private String gdevBy = "https://github.com/gdevby/starter4j";
-    private Label uploadStatus = new Label("upload status");
+    private Label uploadStatus = new Label();
+    private Label uploadWord = new Label();
+    private Label uploadPercent = new Label();
     private ResourceBundle resourceBundle;
 
     public StarterStatusStage(String appName, boolean indeterminate, ResourceBundle resourceBundle) {
         this.resourceBundle = resourceBundle;
         setResizable(false);
-        initStyle(StageStyle.UNDECORATED);
+        initStyle(StageStyle.TRANSPARENT);
         StageUtils.setFavicons(this);
 
         Screen screen = Screen.getPrimary();
         Rectangle2D bounds = screen.getBounds();
         double width = bounds.getWidth() * screen.getOutputScaleX();
         double height = bounds.getHeight() * screen.getOutputScaleY();
-        setWidth(width / 5);
-        setHeight(height / 5);
+        setMinWidth(width / 5);
+        setMinHeight(height / 5);
 
-        BorderPane borderPane = new BorderPane();
-
-        Image image = getImage();
-        if (Objects.nonNull(image)) {
-            BackgroundImage bgImg = new BackgroundImage(
-                    image,
-                    BackgroundRepeat.ROUND,
-                    BackgroundRepeat.NO_REPEAT,
-                    BackgroundPosition.CENTER,
-                    new BackgroundSize(100, 100, true, false, true, false)
-            );
-            borderPane.setBackground(new Background(bgImg));
-        }
-
-        Label label = new Label("app launcher gdev.by");
-        label.setPadding(new Insets(5));
-        Font font = label.getFont();
-        Font newFont = Font.font(font.getFamily(), font.getSize() - 2);
-        label.setFont(newFont);
-        label.setStyle("-fx-font-weight: bold");
-
-        Label nameLabel = new Label(appName);
-        Font nameFont = Font.font(font.getFamily(), font.getSize() + 3);
-        nameLabel.setFont(nameFont);
-        nameLabel.setStyle("-fx-font-weight: bold");
-        nameLabel.setAlignment(Pos.CENTER);
-        nameLabel.setPadding(new Insets(0, 0 , getHeight() / 3 , 0));
-
-        uploadStatus.setStyle("-fx-font-weight: bold");
-        uploadStatus.setAlignment(Pos.CENTER_RIGHT);
-        uploadStatus.setPadding(new Insets(0,3,3,0));
-        uploadStatus.setPrefWidth(getWidth());
-
-        progressBar = new ProgressBar();
-        progressBar.setPrefWidth(getWidth());
-        progressBar.setPadding(Insets.EMPTY);
-        progressBar.setProgress(indeterminate ? ProgressBar.INDETERMINATE_PROGRESS : 0);
-        progressBar.setStyle("-fx-accent: #04b424");
-
-        borderPane.setTop(label);
-        borderPane.setCenter(nameLabel);
-        borderPane.setBottom(uploadStatus);
-
-        BorderPane content = new BorderPane();
-        content.setCenter(borderPane);
-        content.setBottom(progressBar);
-
-        setX(width / 2 - getWidth());
-        setY(height / 2 - getHeight());
-
-        Scene scene = new Scene(content);
-        setScene(scene);
-
-        label.setOnMouseClicked(event -> {
-            if (event.getButton() == MouseButton.PRIMARY) {
-                DesktopUtil.openLink(OSInfo.getOSType(), gdevBy);
-            }
+        setOnShown(event -> {
+            setX(width / 2 - getWidth());
+            setY(height / 2 - getHeight());
         });
 
-        label.setOnMouseEntered(event -> label.setCursor(Cursor.HAND));
+        VBox root = new VBox();
+        root.setSpacing(15);
+        root.setPadding(new Insets(35, 35, 35, 35));
+        root.setAlignment(Pos.CENTER);
+        root.getStyleClass().add("root-status");
 
-        label.setOnMouseExited(event -> label.setCursor(Cursor.DEFAULT));
+        Image image = getImage();
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(100);
+        imageView.setFitHeight(100);
 
+        Label nameLabel = new Label(appName);
+        nameLabel.setPadding(new Insets(0, 0 , 20, 0));
+
+        VBox downloadVBox = new VBox();
+        downloadVBox.setSpacing(5);
+
+        AnchorPane anchorPane = new AnchorPane();
+        AnchorPane.setLeftAnchor(uploadWord, 0.0);
+
+        AnchorPane.setRightAnchor(uploadPercent, 0.0);
+        anchorPane.getChildren().addAll(uploadWord, uploadPercent);
+
+        uploadStatus.setMaxWidth(Double.MAX_VALUE);
+        uploadStatus.setAlignment(Pos.CENTER_RIGHT);
+
+        progressBar = new ProgressBar();
+        progressBar.setPadding(Insets.EMPTY);
+        progressBar.setProgress(indeterminate ? ProgressBar.INDETERMINATE_PROGRESS : 0);
+        progressBar.setMaxWidth(Double.MAX_VALUE);
+
+        downloadVBox.getChildren().addAll(anchorPane, progressBar, uploadStatus);
+
+        root.getChildren().addAll(imageView, nameLabel, downloadVBox);
+
+        Scene scene = new Scene(root, Color.TRANSPARENT);
+        URL stylesheet = getClass().getResource("/style.css");
+        if (Objects.nonNull(stylesheet))
+            scene.getStylesheets().add(stylesheet.toExternalForm());
+        setScene(scene);
     }
 
     private Image getImage() {
-        try (InputStream inputStream = StarterStatusStage.class.getResourceAsStream("/background.jpg")) {
+        try (InputStream inputStream = StarterStatusStage.class.getResourceAsStream("/big-logo.png")) {
             if (inputStream == null) return null;
             return new Image(inputStream);
         } catch (IOException e) {
@@ -144,9 +121,11 @@ public class StarterStatusStage extends Stage {
     private void updateUploadProgressBar(DownloaderStatus status) {
         int uploaded = (int) status.getDownloadSize() / (1024 * 1024);
         int allUpload = (int) status.getAllDownloadSize() / (1024 * 1024);
-        uploadStatus.setText(String.format("%s %s/%s %s ", resourceBundle.getString("uploading"), uploaded, allUpload,
-                resourceBundle.getString("mb")));
-        progressBar.setProgress((double) uploaded / allUpload);
+        double progress = (double) uploaded / allUpload;
+        uploadPercent.setText((int)(progress * 100) + "%");
+        uploadWord.setText(resourceBundle.getString("uploading"));
+        uploadStatus.setText(String.format("%s/%s %s ", uploaded, allUpload, resourceBundle.getString("mb")));
+        progressBar.setProgress(progress);
     }
 
 }
