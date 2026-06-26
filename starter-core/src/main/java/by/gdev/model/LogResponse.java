242c9ccc95bc88113e6f0ef4356e72c1d0538c71
Vadim Semashko <vadim.semashko05@gmail.com>
2026-06-26 09:27:16 +0300

    fixed parsing bug in exe

--- starter-core/src/main/java/by/gdev/model/LogResponse.java
+++ starter-core/src/main/java/by/gdev/model/LogResponse.java
@@ -1,10 +1,14 @@
 package by.gdev.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class LogResponse {
 	String message;
 	String link;
